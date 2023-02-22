4dabb9abee1d7aad2eb9a2c58c5797c04f7b6b46
Dmitry Davydov <109023107+dim1da@users.noreply.github.com>
2023-02-22 11:36:25 +0300

    init

---
+++ index.js
@@ -0,0 +1,9 @@
+console.log (1);
+
+const button = document.querySelector('button')
+const output = document.querySelector('.output')
+
+button.addEventListener('click', function() {
+     output.innerHTML = 'Здесь должна была быть картинка с пакетами, не нашел, как сделать :(';
+     
+});
